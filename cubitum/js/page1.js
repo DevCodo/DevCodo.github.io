20e9ea0eff797b5be616bb7b8b9e1b8344236336
DevCodo <devcododesign@gmail.com>
2019-02-26 20:46:19 +0500

    add

--- cubitum/js/page1.js
+++ cubitum/js/page1.js
@@ -73,6 +73,44 @@ box.addEventListener("mouseover", function(event) {
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function(event) {
         // alpha: rotation around z-axis (от 0 до 360)
+        var z = event.alpha;
+        // gamma: left to right (от -90 до 90)
+        var y = event.gamma;
+        // beta: front back motion (от -180 до 180)
+        var x = event.beta;
+
+        handleOrientationEvent(x, y, z);
+    });
+}
+
+var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+
+    if (x >  90) { x =  90};
+    if (x < -90) { x = -90};
+
+    element1.style.left = coordElement1.left - coordBox.left + y + "px";
+    element1.style.top = coordElement1.top - coordBox.top + x + "px";
+
+    element2.style.left = coordElement2.left - coordBox.left + y  / 20 + "px";
+    element2.style.top = coordElement2.top - coordBox.top + x + "px";
+
+    element3.style.left = coordElement3.left - coordBox.left + y + "px";
+    element3.style.top = coordElement3.top - coordBox.top + x + "px";
+
+    element4.style.left = coordElement4.left - coordBox.left + y + "px";
+    element4.style.top = coordElement4.top - coordBox.top + x + "px";
+
+    element5.style.left = coordElement5.left - coordBox.left + y + "px";
+    element5.style.top = coordElement5.top - coordBox.top + x + "px";
+}    
+
+
+
+
+/* 
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function(event) {
+        // alpha: rotation around z-axis (от 0 до 360)
         var rotateDegrees = event.alpha;
         // gamma: left to right (от -90 до 90)
         var leftToRight = event.gamma;
@@ -123,7 +161,7 @@ var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
     mousStartX = frontToBack;
     mousStartY = leftToRight;
 
-};
+}; */
 
 
 
