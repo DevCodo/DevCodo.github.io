1ef63f7b7e6b96e9cd006dccd53bde2a89d83e5e
DevCodo <devcododesign@gmail.com>
2019-02-27 12:20:22 +0500

    add

--- cubitum/js/page1.js
+++ cubitum/js/page1.js
@@ -31,25 +31,31 @@ var mousStartY;
 var differenceX = 0;
 var differenceY = 0;
 box.addEventListener("mousemove", function(event) {
-
+    resizeWidthelement();
+    coordBox = boxElements.getBoundingClientRect()
+    coordElement1 = element1.getBoundingClientRect()
+    coordElement2 = element2.getBoundingClientRect()
+    coordElement3 = element3.getBoundingClientRect()
+    coordElement4 = element4.getBoundingClientRect()
+    coordElement5 = element5.getBoundingClientRect()
     if (mousStartX && mousStartY) {
 
             differenceX += mousStartX- event.clientX;
             differenceY += mousStartY- event.clientY;
 
-            element1.style.left = coordElement1.left - coordBox.left + differenceX / 30 + "px";
-            element1.style.top = coordElement1.top - coordBox.top + differenceY / 40 + "px";
+            element1.style.left = coordElement1.left - coordBox.left + differenceX / 20 + "px";
+            element1.style.top = coordElement1.top - coordBox.top + differenceY / 20 + "px";
 
-            element2.style.left = coordElement2.left - coordBox.left + differenceX  / 20 + "px";
-            element2.style.top = coordElement2.top - coordBox.top + differenceY  / 30 + "px";
+            element2.style.left = coordElement2.left - coordBox.left + differenceX  / 50 + "px";
+            element2.style.top = coordElement2.top - coordBox.top + differenceY  / 50 + "px";
 
-            element3.style.left = coordElement3.left - coordBox.left + differenceX  / 50 + "px";
-            element3.style.top = coordElement3.top - coordBox.top + differenceY  / 20 + "px";
+            element3.style.left = coordElement3.left - coordBox.left + differenceX  / 30 + "px";
+            element3.style.top = coordElement3.top - coordBox.top + differenceY  / 30 + "px";
 
-            element4.style.left = coordElement4.left - coordBox.left + differenceX  / 40 + "px";
+            element4.style.left = coordElement4.left - coordBox.left + differenceX  / 50 + "px";
             element4.style.top = coordElement4.top - coordBox.top + differenceY  / 30 + "px";
 
-            element5.style.left = coordElement5.left - coordBox.left + differenceX  / 30 + "px";
+            element5.style.left = coordElement5.left - coordBox.left + differenceX  / 60 + "px";
             element5.style.top = coordElement5.top - coordBox.top + differenceY  / 50 + "px";
 
     }
@@ -84,11 +90,16 @@ resizeWidthelement();
 window.addEventListener("resize", resizeWidthelement);
 
 function resizeWidthelement() {
-    element1.style.cssText = `width: ${boxElements.clientWidth * element1СoefWidth}px; height: ${boxElements.clientWidth * element1СoefHeight}px; `;
-    element2.style.cssText = `width: ${boxElements.clientWidth * element2СoefWidth}px; height: ${boxElements.clientWidth * element2СoefHeight}px; `;
-    element3.style.cssText = `width: ${boxElements.clientWidth * element3СoefWidth}px; height: ${boxElements.clientWidth * element3СoefHeight}px; `;
-    element4.style.cssText = `width: ${boxElements.clientWidth * element4СoefWidth}px; height: ${boxElements.clientWidth * element4СoefHeight}px; `;
-    element5.style.cssText = `width: ${boxElements.clientWidth * element5СoefWidth}px; height: ${boxElements.clientWidth * element5СoefHeight}px; `;
+    element1.style.cssText = `width: ${boxElements.clientWidth * element1СoefWidth}px; height: ${boxElements.clientWidth * element1СoefHeight}px;`;
+    element2.style.cssText = `width: ${boxElements.clientWidth * element2СoefWidth}px; height: ${boxElements.clientWidth * element2СoefHeight}px;`;
+    element3.style.cssText = `width: ${boxElements.clientWidth * element3СoefWidth}px; height: ${boxElements.clientWidth * element3СoefHeight}px;`;
+    element4.style.cssText = `width: ${boxElements.clientWidth * element4СoefWidth}px; height: ${boxElements.clientWidth * element4СoefHeight}px;`;
+    element5.style.cssText = `width: ${boxElements.clientWidth * element5СoefWidth}px; height: ${boxElements.clientWidth * element5СoefHeight}px;`;
+    // element1.style.cssText = `width: ${boxElements.clientWidth * element1СoefWidth}px; height: ${boxElements.clientWidth * element1СoefHeight}px; top: 5%; left: 10%;`;
+    // element2.style.cssText = `width: ${boxElements.clientWidth * element2СoefWidth}px; height: ${boxElements.clientWidth * element2СoefHeight}px; top: 0%; left: 60%;`;
+    // element3.style.cssText = `width: ${boxElements.clientWidth * element3СoefWidth}px; height: ${boxElements.clientWidth * element3СoefHeight}px; top: 65%; left: -5%;`;
+    // element4.style.cssText = `width: ${boxElements.clientWidth * element4СoefWidth}px; height: ${boxElements.clientWidth * element4СoefHeight}px; top: 40%; left: 75%;`;
+    // element5.style.cssText = `width: ${boxElements.clientWidth * element5СoefWidth}px; height: ${boxElements.clientWidth * element5СoefHeight}px; top: 40%; left: 35%;`;
 };
 
 /* анимирование объектов на телефоне *//////////////////////////////////////////////////////////////////////////////
