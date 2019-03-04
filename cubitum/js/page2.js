b86617b8ec108b52c7d71be8e7f439112dd6e8d7
DevCodo <devcododesign@gmail.com>
2019-03-04 14:40:33 +0500

    add

--- cubitum/js/page2.js
+++ cubitum/js/page2.js
@@ -5,7 +5,10 @@ $(function() {
 /* переключение кейсов */////////////////////////////////////////////////////////////////////////
 var $sections = $(".case__info__content");
 
-$(".case__nav2").click(function() {
+$(".case__nav2").on("click", nextCase);
+$(".case__nav1").on("click", prevCase);
+
+function nextCase() {
     
     let $sectionNow = $(".case__info__content:not([hidden])");
     let indexNow = $(".case__info__content").index($sectionNow);
@@ -17,9 +20,9 @@ $(".case__nav2").click(function() {
         $(`.case__info__content:eq(0), .case__description__content:eq(0)`).prop("hidden", false);
         $(`.case-show__mackbook__iframe, .case-show__iphone__iframe`).attr("src", $sections.eq(0).attr("src"));
     }
-})
+};
 
-$(".case__nav1").click(function() {
+function prevCase() {
     
     let $sectionNow = $(".case__info__content:not([hidden])");
     let indexNow = $(".case__info__content").index($sectionNow);
@@ -32,7 +35,40 @@ $(".case__nav1").click(function() {
         $(`.case-show__mackbook__iframe, .case-show__iphone__iframe`).attr("src", $sections.eq($sections.length - 1).attr("src"));
     }
 
-})
+};
+
+/* переключение кейсов свайпом*/////////////////////////////////////////////////////////////////////////
+
+// Вешаем на прикосновение функцию handleTouchStart
+$(".case").on('touchstart', handleTouchStart);
+// А на движение пальцем по экрану - handleTouchMove      
+$(".case").on('touchmove', handleTouchMove);
+
+var xDown = null;                                                                                                              
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                                                          
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown) return;
+
+    var xUp = evt.touches[0].clientX;                                    
+
+    var xDiff = xDown - xUp;
+
+        if ( xDiff > 30 ) {
+            /* left swipe */ 
+            nextCase();
+        } else if ( xDiff < -30 ){
+            /* right swipe */
+            prevCase();
+        }                       
+
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 /* управление шириной iframe */////////////////////////////////////////////////////////////////////////
 var $iframeMac = $(".case-show__mackbook__iframe");
