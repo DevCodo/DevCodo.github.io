5dabb4e96de5eea89132238b749d5ed2eb8a2efd
DevCodo <devcododesign@gmail.com>
2019-04-12 18:49:19 +0500

    a

---
+++ react_app2/src/components/App/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Header from '../Header';
+import ItemList from '../ItemList';
+import RandomPlanet from '../RandomPlanet';
+import PersonDetails from '../PersonDetails';
+
+import './App.scss';
+
+class App extends React.Component {
+
+    state = {
+        showRandomPlanet: true,
+        selectedPerson: 5
+    }
+
+    onPersonSelected = (id) => {
+        this.setState({
+            selectedPerson: id
+        })
+    }
+
+    render() {
+        
+        return (
+            <div>
+                <Header />
+                <RandomPlanet />
+    
+                <div className='row mb2'>
+                    <div className='col-md-6'>
+                        <ItemList onItemSelected={this.onPersonSelected}/>
+                    </div>
+                    <div className='col-md-6'>
+                        <PersonDetails personId={this.state.selectedPerson} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
--- react_app2/src/index.js
+++ react_app2/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import '@babel/polyfill';
 import 'fetch-polyfill';
-import './scss/main.scss';
+import './scss/main.scss'
+
+import App from './components/App'
 
 
+
+ReactDOM.render(<App />, document.querySelector('#root'));
