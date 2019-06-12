3200364fbf14a0a3de4ef7a245b28ae279b674dc
DevCodo <devcododesign@gmail.com>
2019-06-12 13:09:58 +0500

    aa

---
+++ Beton/common.js
@@ -0,0 +1,11 @@
+// $(".line").css({
+//   height: $(".box__car").height() * 0.01,
+//   width: $(".box__car").width() * 0.2
+// })
+
+// window.addEventListener("resize", function() {
+//   $(".line").css({
+//     height: $(".box__car").height() * 0.01,
+//     width: $(".box__car").width() * 0.2
+//   })
+// })
