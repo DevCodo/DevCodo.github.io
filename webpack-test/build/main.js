3eaa980f0d4864bcbf684bc3ee8ad9e782e04690
DevCodo <devcododesign@gmail.com>
2019-08-05 18:49:47 +0500

    aa

---
+++ webpack-test/build/main.js
@@ -0,0 +1,109 @@
+import "core-js/modules/es.symbol";
+import "core-js/modules/es.symbol.description";
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.symbol.to-string-tag";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.json.to-string-tag";
+import "core-js/modules/es.math.to-string-tag";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+!function (e) {
+  var t = {};
+
+  function n(r) {
+    if (t[r]) return t[r].exports;
+    var o = t[r] = {
+      i: r,
+      l: !1,
+      exports: {}
+    };
+    return e[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports;
+  }
+
+  n.m = e, n.c = t, n.d = function (e, t, r) {
+    n.o(e, t) || Object.defineProperty(e, t, {
+      enumerable: !0,
+      get: r
+    });
+  }, n.r = function (e) {
+    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+      value: "Module"
+    }), Object.defineProperty(e, "__esModule", {
+      value: !0
+    });
+  }, n.t = function (e, t) {
+    if (1 & t && (e = n(e)), 8 & t) return e;
+    if (4 & t && "object" == _typeof(e) && e && e.__esModule) return e;
+    var r = Object.create(null);
+    if (n.r(r), Object.defineProperty(r, "default", {
+      enumerable: !0,
+      value: e
+    }), 2 & t && "string" != typeof e) for (var o in e) {
+      n.d(r, o, function (t) {
+        return e[t];
+      }.bind(null, o));
+    }
+    return r;
+  }, n.n = function (e) {
+    var t = e && e.__esModule ? function () {
+      return e.default;
+    } : function () {
+      return e;
+    };
+    return n.d(t, "a", t), t;
+  }, n.o = function (e, t) {
+    return Object.prototype.hasOwnProperty.call(e, t);
+  }, n.p = "", n(n.s = 0);
+}([function (e, t, n) {
+  "use strict";
+
+  n.r(t);
+  var r = new (
+  /*#__PURE__*/
+  function () {
+    function _class() {
+      _classCallCheck(this, _class);
+    }
+
+    _createClass(_class, [{
+      key: "add",
+      value: function add() {
+        for (var _len = arguments.length, e = new Array(_len), _key = 0; _key < _len; _key++) {
+          e[_key] = arguments[_key];
+        }
+
+        return e.reduce(function (e, t) {
+          return e + t;
+        }, 0);
+      }
+    }]);
+
+    return _class;
+  }())();
+  new (
+  /*#__PURE__*/
+  function () {
+    function _class2() {
+      _classCallCheck(this, _class2);
+    }
+
+    _createClass(_class2, [{
+      key: "log",
+      value: function log(e) {
+        console.log("================="), console.log(e), console.log("=================");
+      }
+    }]);
+
+    return _class2;
+  }())().log(r.add(1, 2, 3));
+}]);
