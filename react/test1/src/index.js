c3bc8d4be7546369a12d3fec610903655b937d8e
DevCodo <devcododesign@gmail.com>
2019-08-07 14:22:46 +0500

    aa

--- react/test1/src/index.js
+++ react/test1/src/index.js
@@ -1,2 +1,45 @@
-import "./scss/main.scss";
+import "@babel/polyfill";
 
+// function* gen(num) {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+
+// }
+
+// let same = gen();
+
+// let osnova = Math.floor(num / 10) ; // 133
+// let ostatok = num % 10;             // 4
+
+
+
+function num(number) {
+  let num = number;
+
+  while( num > 0 ) {
+    let osn = Math.floor(num / 10) ; // 133
+    let ost = num % 10;
+    console.log(ost);
+    num = osn;
+  }
+}
+
+function* gen(number) {
+  let num = number;
+
+  while( num > 0 ) {
+    let osn = Math.floor(num / 10) ; // 133
+    let ost = num % 10;
+    num = osn;
+    yield ost;
+  }
+}
+
+let num3 = gen(987484363);
+
+for (let some of num3 ) {
+  console.log(some);
+}
+
+// num(987654321);
