7734f62fb506a13600c19103c9384b1f49a0c780
DevCodo <devcododesign@gmail.com>
2019-09-05 22:23:29 +0500

    df

---
+++ project/StarWars/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+    <ErrorIndicator />
+    
+  </div>
+</template>
+
+<script>
+import ErrorIndicator from './components/ErrorIndicator'
+
+export default {
+  components: {
+    ErrorIndicator
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
