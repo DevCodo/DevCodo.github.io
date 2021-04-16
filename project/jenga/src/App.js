415d29af9c77fe893b1d270bb9030ed8e97d29d7
DevCodo <devcododesign@gmail.ru>
2021-04-16 09:08:31 +0500

    add build

--- project/jenga/src/App.js
+++ project/jenga/src/App.js
@@ -1,12 +1,18 @@
-import { Canvas } from 'react-three-fiber'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
-import { MOUSE } from 'three'
 import { Physics, useBox, usePlane } from '@react-three/cannon'
+import { MOUSE } from 'three'
 
 const Box = () => {
   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }))
   return (
-    <mesh ref={ref} position={[0, 2, 0]} onClick={() => api.velocity.set(1, 2, 1)}>
+    <mesh
+      ref={ref}
+      castShadow
+      receiveShadow
+      position={[0, 2, 0]}
+      onClick={() => api.velocity.set(1, 2, 1)}
+    >
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="lightblue" />
     </mesh>
@@ -16,7 +22,7 @@ const Box = () => {
 const Plane = () => {
   const [ref] = usePlane(() => ({ rotation: [Math.PI * 1.5, 0, 0] }))
   return (
-    <mesh ref={ref} position={[0, 0, 0]} rotation={[Math.PI * 1.5, 0, 0]}>
+    <mesh receiveShadow ref={ref} position={[0, 0, 0]} rotation={[Math.PI * 1.5, 0, 0]}>
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
       <meshLambertMaterial attach="material" color="green" />
     </mesh>
@@ -26,9 +32,9 @@ const Plane = () => {
 const App = () => {
   return (
     <Canvas
-      frameloop="always"
-      // shadowMap
-      // colorManagement
+      concurrent
+      shadowMap
+      sRGB
       gl={{ alpha: false }}
       camera={{ position: [-1, 2, 4], fov: 100 }}
     >
@@ -40,6 +46,7 @@ const App = () => {
       />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
+      <pointLight position={[5, 5, 5]} intensity={0.9} />
       <Physics>
         <Box />
         <Plane />
